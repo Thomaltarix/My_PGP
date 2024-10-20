0731ee42177c8304f17075e1a708675ab30077ad
thomaltarix <thomas.boue@epitech.eu>
2024-10-20 13:37:42 +0200

    fix: Fix the decryption of the pgp algorithms

--- src/pgp_xor.py
+++ src/pgp_xor.py
@@ -11,17 +11,25 @@ from src.xor import xor
 from src.rsa import rsa
 
 def encrypt_pgp_xor(message, key, block_mode, left, right) -> str:
-    xor_key, rsa_key = key.split(":")
-    new_left, new_right = rsa_key.split("-")
-    encrypted_message = xor(message, xor_key, True, block_mode, "", "")
-    encrypted_key = rsa(xor_key, rsa_key, True, block_mode, new_left, new_right)
+    try:
+        xor_key, rsa_key = key.split(":")
+        new_left, new_right = rsa_key.split("-")
+        encrypted_message = xor(message, xor_key, True, block_mode, "", "")
+        encrypted_key = rsa(xor_key, rsa_key, True, block_mode, new_left, new_right)
+    except Exception as e:
+        print(e)
+        exit(84)
     return encrypted_key + "\n" + encrypted_message
 
 def decrypt_pgp_xor(message, key, block_mode, left, right) -> str:
-    xor_key, rsa_key  = key.split(":")
-    new_left, new_right = rsa_key.split("-")
-    decrypted_key = rsa(xor_key, rsa_key, False, block_mode, new_left, new_right)
-    decrypted_message = xor(message, decrypted_key, False, block_mode, "", "")
+    try:
+        xor_key, rsa_key  = key.split(":")
+        new_left, new_right = rsa_key.split("-")
+        decrypted_key = rsa(xor_key, rsa_key, False, block_mode, new_left, new_right).split("\n")[0]
+        decrypted_message = xor(message, decrypted_key, False, block_mode, "", "")
+    except Exception as e:
+        print(e)
+        exit(84)
     return decrypted_message
 
 def pgp_xor(message, key, encrypt, block_mode, left, right) -> str:
